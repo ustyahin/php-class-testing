d1679997da49e7f70e075ad8b774a5596c003ee6
ustyahin <ustyahin.egor@gmail.com>
2022-07-10 21:29:36 +0700

    Update class.php

--- class.php
+++ class.php
@@ -2,14 +2,14 @@
 
 class Database
 {
-    public $link;
+   public $link;
 
    public function connect()
    {
         $this->link = mysqli_connect("localhost", "root", "");
-        mysqli_select_db($link, "class");
+        mysqli_select_db($this->link, "class");
 
-        if ($link == false) {
+        if ($this->link == false) {
             exit("Ошибка подключения к БД");
         }
    } 
